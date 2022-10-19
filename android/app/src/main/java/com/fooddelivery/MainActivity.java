503600d858edc98e658ee68d12bd299948d013a4
angelinemarym <angeline030303@gmail.com>
2022-10-19 14:37:57 +0700

    feat: setup stack navigation

--- android/app/src/main/java/com/fooddelivery/MainActivity.java
+++ android/app/src/main/java/com/fooddelivery/MainActivity.java
@@ -3,6 +3,7 @@ package com.fooddelivery;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
 
@@ -13,6 +14,11 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "FoodDelivery";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 
   /**
---
+++ src/routes/Router.js
@@ -0,0 +1,22 @@
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {Home} from '../views';
+
+const Stack = createNativeStackNavigator();
+const Router = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Router;
