2682e591d30f7200c2fdc462a5facffa34857599
angelinemarym <angeline030303@gmail.com>
2022-12-13 21:10:16 +0700

    feat: create combine icon file

---
+++ src/assets/icons/index.js
@@ -0,0 +1,7 @@
+import IconCart from './ic-cart';
+import IconChat from './ic-chat';
+import IconFavorite from './ic-favorite';
+import IconHome from './ic-home';
+import IconProfile from './ic-profile';
+
+export {IconCart, IconChat, IconFavorite, IconHome, IconProfile};
